13cfb2c477de50c858a8e03cc7b9cadde0b8725a
Artemiy <senartemiy@gmail.com>
2020-10-08 14:21:34 +0300

    Initial commit

---
+++ Calculator_Senkevich/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_Senkevich
+{   
+    public partial class MainWindow : Window
+    {
+        Controller controller;
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new Controller();
+        }
+
+		private void CalculateButton_Click(object sender, RoutedEventArgs e) // Получение значений
+		{
+			string text1 = Num1TextBox.Text.Trim();
+			string text2 = Num2TextBox.Text.Trim();
+			double num1 = 0;
+			double num2 = 0;
+			if (!string.IsNullOrEmpty(text1) && !string.IsNullOrEmpty(text2))
+			{
+				try
+				{
+					num1 = Convert.ToInt32(text1);
+					num2 = Convert.ToInt32(text2);
+				}
+				catch (Exception exc) { }
+				Calculate(num1, num2);
+
+				ResultTextBlock.Text = controller.result.ToString(); // Обновить viev
+			}
+		}
+
+		public void Calculate(double num1, double num2) // Отправка знаений в модель
+		{
+			switch (OperatorType.Text)
+			{
+				case "+":
+					controller.Add(num1, num2);
+					break;
+				case "-":
+					controller.Subtraction(num1, num2);
+					break;
+				case "*":
+					controller.Multiplication(num1, num2);
+					break;
+				case "/":
+					controller.Division(num1, num2);
+					break;
+				case "%":
+					controller.percent(num1, num2);
+					break;
+				case "^":
+					controller.Pow(num1, num2);
+					break;
+				case "log":
+					controller.Log10(num1);
+					break;
+				case "Sqrt":
+					controller.Sqrt(num1);
+					break;
+			}
+		}
+
+	}
+}
